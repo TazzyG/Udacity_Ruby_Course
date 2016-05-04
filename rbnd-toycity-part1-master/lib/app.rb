a40ef1a44bf9235e6ff583b9a157a4f5db9acb90
Laurie <gloge@haathi-consulting.com>
2016-05-04 11:30:02 -0600

    got products working

--- rbnd-toycity-part1-master/lib/app.rb
+++ rbnd-toycity-part1-master/lib/app.rb
@@ -19,25 +19,33 @@ puts "|_|                                       "
 products_hash["items"].each do |toy|
   # Print the name of the toy
   puts toy["title"]
-  # Print the name of the toy
-  puts toy["description"]
+  puts "*" * 20
   # Print the retail price of the toy
-  retail_price = toy["full-price"]
-  puts  retail_price
+  retail_price =  toy["full-price"]
+  puts  "Retail Price: $#{retail_price}"
   # Calculate and print the total number of purchases
   purchases = toy["purchases"].length
-  puts purchases
-  # Calculate and print the total amount of sales
-  sales = toy["purchases"]["price".to_i]["price"]
-  sales_total = sales += sales
-  puts sales_total
+  puts "Total Purchases: #{purchases}"
+  # Loop through purchases to find total sales
+  sales = 0
+  toy["purchases"]["price".to_i]["price"]
+  toy["purchases"].each do |purchase|
+  sales += purchase["price"] 
+  end
+  puts "Total Sales: $#{sales}"
+   
+  
   # Calculate and print the average price the toy sold for
-  average_price = sales_total/purchases
-  puts average_price
+  average_price = sales/purchases
+  puts "Average Price: $#{average_price}"
   # Calculate and print the average discount (% or $) based off the average sales price
-  discount = retail_price.to_i - average_price
-  puts discount.round(2)
-
+  discount = (retail_price.to_f - average_price.to_f).round(2)
+  puts "Average Discount: $#{discount}"
+  discount_percentage = (discount.to_f/retail_price.to_f)*100
+  puts "Average Discount Percentage: " + discount_percentage.round(2).to_s + "%"
+  puts "*" * 20 
+  puts ""
+end
 
 
 	puts " _                         _     "
@@ -51,33 +59,3 @@ products_hash["items"].each do |toy|
 # For each brand in the data set:
 
 
-brands = products_hash["items"].each do |item|
-  
-  # Print the name of the brand
-    puts item["brand"]
-    # Count and print the number of the brand's toys we stock
-    puts item["stock"]
-    # Calculate and print the average price of the brand's toys
-    # Factors for Calculating average price
-    # Price
-    price = item["purchases"]["price".to_i]["price"]
-    # Aggregate total based on price of brand
-    price_total = price += price
-    # Number of items purchased
-    sales = item["purchases"].length
-    # Calculation for Average Price
-    average_price = price_total/sales
-    puts average_price
-    # Calculate and print the total revenue of all the brand's toy sales combined
-    # Factors for Calculating total revenue of brand
-    retail_price = item["full-price"].to_i 
-    gross_revenue = sales * retail_price
-    cost = sales * average_price
-    revenue = gross_revenue - cost
-    puts revenue.round(2)
-    puts "---------------------------------------------------------------"
-  
-
-  end
-  
-end
